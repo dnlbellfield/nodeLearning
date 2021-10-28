7791c44f47c81414b2792d25de7e62c961a64d39
Daniel Bellfield <dnlbellfield@gmail.com>
2021-10-28 16:17:26 -0700

    added message.txt and greet-to-file

--- greet-to-file.js
+++ greet-to-file.js
@@ -1,8 +1,15 @@
 import { writeFile } from 'fs';
-import { Buffer } from 'buffer';
 
-const data = new Uint8Array(Buffer.from('Hello Node.js'));
-writeFile('message.txt', data, (err) => {
-  if (err) throw err;
-  console.log('The file has been saved!');
-});
+fs.writeFile('greeting.txt', "Hello World", err => console.log("Error occurred when writing the file.") )
+
+
+
+
+// import { writeFile } from 'fs';
+// import { Buffer } from 'buffer';
+
+// const data = new Uint8Array(Buffer.from('Hello Node.js'));
+// writeFile('message.txt', data, (err) => {
+//   if (err) throw err;
+//   console.log('The file has been saved!');
+// });
