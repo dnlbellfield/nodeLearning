030c6e08107c98eb6c162ddceac1818e8e0d91a6
Daniel Bellfield <dnlbellfield@gmail.com>
2021-10-28 16:12:34 -0700

    add greet-to-file lern to save

---
+++ greet-to-file.js
@@ -0,0 +1,8 @@
+import { writeFile } from 'fs';
+import { Buffer } from 'buffer';
+
+const data = new Uint8Array(Buffer.from('Hello Node.js'));
+writeFile('message.txt', data, (err) => {
+  if (err) throw err;
+  console.log('The file has been saved!');
+});
