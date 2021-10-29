c450da8d5e9d59f7f7e40e7dcbdb419e6afdb109
Daniel Bellfield <dnlbellfield@gmail.com>
2021-10-28 17:30:52 -0700

    update

--- greet-to-file.js
+++ greet-to-file.js
@@ -1,4 +1,8 @@
 import { writeFile } from 'fs';
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'process';
+
+
 // create var, set it to a funcion, which will be callable.
 const writeGreetingToFile = (name) => {
   writeFile('greeting.txt', `Hello, ${name}`,(err) => {
@@ -8,14 +12,17 @@ const writeGreetingToFile = (name) => {
 
 }
 
+const rl = readline.createInterface({ input, output });
+const answer = await rl.question('What is your name? ');
+
+writeFile('greeting.txt', `Hello, ${answer}`,(err) => {
+  if (err) throw err;
+  console.log(`Thank you for your name, ${answer}. The file has been saved at greeting.txt!`);
+})
+
+rl.close();
+
+
+
 // call it
-writeGreetingToFile("Daniel");
-
-// import { writeFile } from 'fs';
-// import { Buffer } from 'buffer';
-
-// const data = new Uint8Array(Buffer.from('Hello Node.js'));
-// writeFile('message.txt', data, (err) => {
-//   if (err) throw err;
-//   console.log('The file has been saved!');
-// });
+//writeGreetingToFile("Daniel");
